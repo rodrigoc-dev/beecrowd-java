ac4171e31d18fcd9d48f2a2bfaffb899f8bc5c8e
Rodrigo Castro <rodrigo.castro1318@gmail.com>
2026-09-14 17:20:28 -0300

    fix: corrige calculo da media ponderada no exercicio 1006

--- src/iniciante/ex1006/Main.java
+++ src/iniciante/ex1006/Main.java
@@ -1,11 +1,34 @@
 package iniciante.ex1006;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
-        double A, B, C;
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        double A, B, C, peso1, peso2, peso3;
+        double pesoTotal, calcPeso1, calcPeso2, calcPeso3, mediaAluno;
 
-        System.out.println("MEDIA = ");
+        peso1 = 2;
+        peso2 = 3;
+        peso3 = 5;
+        pesoTotal = peso1 + peso2 + peso3;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        C = sc.nextDouble();
+
+        calcPeso1 = A * peso1;
+        calcPeso2 = B * peso2;
+        calcPeso3 = C * peso3;
+
+        mediaAluno = (calcPeso1 + calcPeso2 + calcPeso3) / pesoTotal;
+
+        System.out.printf("MEDIA = %.1f%n", mediaAluno);
+
+        sc.close();
 
     }
 }

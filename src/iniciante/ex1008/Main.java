a28fd5324a74dd48a074e8eebbdc48038a919c61
Rodrigo Castro <rodrigo.castro1318@gmail.com>
2026-09-15 15:22:34 -0300

    feat: adiciona exercicio 1008 ao projeto

---
+++ src/iniciante/ex1008/Main.java
@@ -0,0 +1,25 @@
+package iniciante.ex1008;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numFunc, hrsTrab;
+        double valorHora, salario;
+
+        numFunc = sc.nextInt();
+        hrsTrab = sc.nextInt();
+        valorHora = sc.nextDouble();
+
+        salario = hrsTrab * valorHora;
+
+        System.out.println("NUMBER = " + numFunc);
+        System.out.printf("SALARY = U$ %.2f%n", salario);
+
+        sc.close();
+    }
+}
